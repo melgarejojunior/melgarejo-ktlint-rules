877ca8f7becd20fc8e63605ea0dd319d5ddb86de
William <william@jera.com.br>
2019-12-08 16:32:40 -0400

    Cleaning

--- src/main/kotlin/ktlintrules/MethodsOrderingRule.kt
+++ src/main/kotlin/ktlintrules/MethodsOrderingRule.kt
@@ -2,14 +2,11 @@ package ktlintrules
 
 import com.pinterest.ktlint.core.Rule
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
-import org.jetbrains.kotlin.lexer.KtTokens
-import org.jetbrains.kotlin.psi.KtAnnotationEntry
+import org.jetbrains.kotlin.lexer.KtTokens.*
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.psiUtil.isPublic
-import org.jetbrains.kotlin.psi.stubs.KotlinModifierListStub
 import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes
-import org.jetbrains.kotlin.util.capitalizeDecapitalize.capitalizeFirstWord
 
 class MethodsOrderingRule : Rule("methods-ordering-rule") {
     override fun visit(
@@ -33,13 +30,13 @@ class MethodsOrderingRule : Rule("methods-ordering-rule") {
         return with(function) {
             when {
                 isPublic -> Modifiers.PUBLIC
-                hasModifier(KtTokens.OVERRIDE_KEYWORD) -> Modifiers.OVERRIDE
-                hasModifier(KtTokens.INTERNAL_KEYWORD) -> Modifiers.INTERNAL
-                hasModifier(KtTokens.PROTECTED_KEYWORD) -> Modifiers.PROTECTED
-                hasModifier(KtTokens.OPEN_KEYWORD) -> Modifiers.OPEN
-                hasModifier(KtTokens.ABSTRACT_KEYWORD) -> Modifiers.ABSTRACT
-                hasModifier(KtTokens.PRIVATE_KEYWORD) -> Modifiers.PRIVATE
-                hasModifier(KtTokens.INLINE_KEYWORD) -> Modifiers.INLINE
+                hasModifier(OVERRIDE_KEYWORD) -> Modifiers.OVERRIDE
+                hasModifier(INTERNAL_KEYWORD) -> Modifiers.INTERNAL
+                hasModifier(PROTECTED_KEYWORD) -> Modifiers.PROTECTED
+                hasModifier(OPEN_KEYWORD) -> Modifiers.OPEN
+                hasModifier(ABSTRACT_KEYWORD) -> Modifiers.ABSTRACT
+                hasModifier(PRIVATE_KEYWORD) -> Modifiers.PRIVATE
+                hasModifier(INLINE_KEYWORD) -> Modifiers.INLINE
                 else -> Modifiers.PUBLIC
             }
         }
